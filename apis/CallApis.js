83244364ad6bb3c85a04cde9f0fee1b38055895f
Giomayra Rosales <giomayra.rosales@gmail.com>
2020-12-15 14:44:45 +0100

    commit api lord of the rings

--- apis/CallApis.js
+++ apis/CallApis.js
@@ -16,6 +16,40 @@ const dogAPIRequest = async () => {
     return await fetch(path,{});
 }
 
+
+
+const lotrAPIRequest = async () => {
+    //"https://api.thedogapi.com/v1/breeds"
+    const basePathAPI = "https://the-one-api.dev";
+    let path = `${basePathAPI}/v2/character`;
+    console.log(path);
+    return await fetch(path,{
+        method: "GET",
+        headers: {"Authorization": "Bearer X-StUdZtSwsPPOGCy1kU "}
+    });
+}
+
+const callLotrAPI = async() => {
+    try {
+        let lotrAPIResponse = lotrAPIRequest();
+        console.log(lotrAPIResponse);
+        lotrAPIResponse
+            .then((response) => {
+                console.log(response);
+                return (response.json());
+            })
+            .then((data) => {
+                console.log(data);
+                let index =  getRandomInt(0, data.length);
+              /*  displayModal(data[index]);*/
+            })
+            .catch(error => console.error(error));
+    } catch(error) {
+        console.error(error);
+    }
+};
+
+
 const callDogAPI = async() => {
     try {
         let dogAPIResponse = dogAPIRequest();
@@ -49,9 +83,11 @@ const displayModal = (dog) => {
     modalImg.src = dog.image.url;
 }
 
-
 Array.from(document.querySelectorAll(".greensquare")).forEach(square =>
-    square.addEventListener("click", callDogAPI) );
+    square.addEventListener("click", callLotrAPI) );
+
+/*Array.from(document.querySelectorAll(".greensquare")).forEach(square =>
+    square.addEventListener("click", callDogAPI) );*/
 
 
 document.querySelector(".modal-close").addEventListener("click", () => {
