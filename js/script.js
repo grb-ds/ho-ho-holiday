7357ff487377543bea09ebdfcb95d1fcce97f90b
adrienclesse <adrienclesse@hotmail.com>
2020-12-16 11:14:27 +0100

    Reorganising folders, settings dice and set a grid

--- js/script.js
+++ js/script.js
@@ -132,22 +132,23 @@ document.querySelector("#reset").addEventListener("click", () =>{
 })
 
 function rollDice() {
-    const dice = [...document.querySelectorAll(".die-list")];
-    dice.forEach(die => {
-      toggleClasses(die);
-      die.dataset.roll = getRandomNumber(1, 6);
-    });
+	const dice = [...document.querySelectorAll(".die-list")];
+	dice.forEach(die => {
+	  toggleClasses(die);
+	  die.dataset.roll = getRandomNumber(1, 6);
+	});
   }
   
   function toggleClasses(die) {
-    die.classList.toggle("odd-roll");
-    die.classList.toggle("even-roll");
+	die.classList.toggle("odd-roll");
+	die.classList.toggle("even-roll");
   }
   
   function getRandomNumber(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   
-  document.getElementById("dice").addEventListener("click", rollDice);
+  document.getElementById("roll-button").addEventListener("click", rollDice);
+  
