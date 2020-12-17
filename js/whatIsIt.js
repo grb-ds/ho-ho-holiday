3d76d9ae61f9b3060b83b9d85cc7ecf13110a22b
Maureen Oldyck <maureen.oldyck@gmail.com>
2020-12-17 09:21:37 +0100

    what is it game code

---
+++ js/whatIsIt.js
@@ -0,0 +1,115 @@
+const randomPicture = (array) => {
+    let image = array[Math.floor(Math.random() * array.length)];
+    document.getElementById("image").src = image.src;
+    return image;
+};
+
+let guesses = 0;
+
+const winOrLose = (guess, answer, modal) => {
+
+        if (guess == answer) {
+            alert("You win! Nice, now you get 3 points!");
+            modal.style.display = "none";
+        } else if (guesses > 3) {
+            alert("You lose, the answer was " + answer + ". Sadly, you don't get points!");
+            modal.style.display = "none";
+        } else {
+            alert("You have " + (4 - guesses) + " left.")
+            guesses++;
+        }
+};
+
+
+const displayModal = (modal) => {
+    // Get the modal
+    modal.style.display = "block";
+}
+
+
+let images = [{
+    name: "my precious",
+    src: "../images/guesstheimage/my-precious.jpeg"
+}, {
+    name: "frodo",
+    src: "../images/guesstheimage/frodo.jpeg",
+}, {
+    name: "the shire",
+    src: "../images/guesstheimage/the-shire.jpeg",
+}, {
+    name: "smeagol",
+    src: "../images/guesstheimage/smeagol.jpeg",
+}, {
+    name: "gandalf",
+    src: "../images/guesstheimage/gandalf.jpeg",
+}, {
+    name: "hobbit",
+    src: "../images/guesstheimage/hobbit.jpeg",
+}, {
+    name: "elves",
+    src: "../images/guesstheimage/elves.jpeg",
+}, {
+    name: "trilogy",
+    src: "../images/guesstheimage/trilogy.jpeg",
+}, {
+    name: "sword",
+    src: "../images/guesstheimage/sword.jpeg",
+}, {
+    name: "dwarves",
+    src: "../images/guesstheimage/dwarves.jpeg",
+}, {
+    name: "beard",
+    src: "../images/guesstheimage/beard.jpeg",
+}, {
+    name: "halfling",
+    src: "../images/guesstheimage/halfling.jpeg",
+}, {
+    name: "mordor",
+    src: "../images/guesstheimage/mordor.jpeg",
+}, {
+    name: "bow and arrow",
+    src: "../images/guesstheimage/bow-and-arrow.jpeg",
+}, {
+    name: "forest",
+    src: "../images/guesstheimage/forest.jpeg",
+}, {
+    name: "eye",
+    src: "../images/guesstheimage/eye.jpeg",
+}, {
+    name: "orcs",
+    src: "../images/guesstheimage/orcs.jpeg",
+}, {
+    name: "smaug",
+    src: "../images/guesstheimage/smaug.jpeg",
+}, {
+    name: "dragon",
+    src: "../images/guesstheimage/dragon.jpeg",
+}, {
+    name: "wizard",
+    src: "../images/guesstheimage/wizard.jpeg",
+}, {
+    name: "ax",
+    src: "../images/guesstheimage/ax.jpeg",
+}, {
+    name: "adventure",
+    src: "../images/guesstheimage/adventure.jpeg",
+}, {
+    name: "hero",
+    src: "../images/guesstheimage/hero.jpeg",
+}];
+
+let randomImage = randomPicture(images);
+
+
+document.querySelector(".click").addEventListener("click", () => {
+    displayModal(myModal)
+});
+
+
+
+
+document.getElementById("submit").addEventListener("click", (event) => {
+    event.preventDefault();
+    let inputGuess = document.querySelector("#guess").value;
+    winOrLose(inputGuess, randomImage.name, myModal);
+});
